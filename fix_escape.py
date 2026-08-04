a192237274acfd2d75111871acbf64522a5692f9
JohnGF <joaot.tomas415@gmail.com>
2026-08-04 11:11:36 +0100

    everythign seemst o work

--- fix_escape.py
+++
@@ -1,7 +0,0 @@
-with open("src/core/exporters/cocitation_exporter.py", "r") as f:
-    content = f.read()
-
-# Let's fix the invalid escape sequences
-content = content.replace('\\href', '\\\\href')
-with open("src/core/exporters/cocitation_exporter.py", "w") as f:
-    f.write(content)
--- fix_latex3.py
+++
@@ -1,7 +0,0 @@
-with open("src/core/exporters/cocitation_exporter.py", "r") as f:
-    content = f.read()
-
-content = content.replace('{cnt:,} \\"', '{cnt:,} \\\\\\\\"')
-
-with open("src/core/exporters/cocitation_exporter.py", "w") as f:
-    f.write(content)
